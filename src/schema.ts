7f149c6a0bd0e7e3d3238ef0a6bd775e20c1455a
Kevin Wang <kwangsan@gmail.com>
2019-12-10 17:00:10 -0500

    extend schema - AuthPayload - Mutation   - signup   - login

--- src/schema.ts
+++ src/schema.ts
@@ -10,7 +10,32 @@ export const typeDefs = gql`
     last_name: String
   }
 
+  type AuthPayload {
+    token: String
+    user: User
+  }
+
   type Query {
     getFirstUser: User
+  }
+
+  type Mutation {
+    """
+    # signup
+    This mutation creates a new row in the **Users** table
+
+    Other entity columns:
+      - \`id\` is auto-incremented
+      - \`type\` defaults to "User"
+    """
+    signup(
+      email: String!
+      password: String!
+      username: String!
+      firstName: String!
+      lastName: String!
+    ): AuthPayload
+
+    login(email: String!, password: String!): AuthPayload
   }
 `
