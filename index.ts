871a32432a6e00b96f48fe6e2aed90055c73a108
Kevin Wang <kwangsan@gmail.com>
2019-12-10 18:28:43 -0500

    delete unneeded src/index

--- index.ts
+++ index.ts
@@ -1,22 +1,33 @@
+import "reflect-metadata"
+
 import { ApolloServer } from "apollo-server"
+import { createConnection } from "typeorm"
 
 import { typeDefs } from "./src/schema"
 import { resolvers } from "./src/resolvers"
-import { connection } from "./src/index"
 
-const server = new ApolloServer({
-  typeDefs: typeDefs,
-  resolvers: resolvers,
-  introspection: true,
-  playground: true,
-  context: request => ({
-    ...request,
-    connection,
-  }),
-})
+async function main() {
+  const connection = await createConnection()
+  console.log("✔ Connected to RDS")
+  const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+    introspection: true,
+    playground: true,
+    context: request => ({
+      ...request,
+      connection,
+    }),
+    dataSources: () => ({
+      connection: connection,
+    }),
+  })
 
-server.listen({ port: 4044 }).then(({ url, subscriptionsUrl }) => {
-  console.log(`🚀 Server ready at ${url}`)
-  console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`)
-  console.log("\n")
-})
+  server.listen({ port: 4044 }).then(({ url, subscriptionsUrl }) => {
+    console.log(`🚀 Server ready at ${url}`)
+    console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`)
+    console.log("\n")
+  })
+}
+
+main()
