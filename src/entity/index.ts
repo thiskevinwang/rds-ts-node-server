5db769dc26c6d8bda35e8cd816a068eaa438b45c
Kevin Wang <kwangsan@gmail.com>
2019-12-14 14:30:14 -0500

    export `entities` from entity - pass this to inline TypeORM connection config

---
+++ src/entity/index.ts
@@ -0,0 +1,10 @@
+import { User } from "./User"
+import { Comment } from "./Comment"
+import { Reaction } from "./Reaction"
+
+// TODO
+// export { User } from "./User"
+// export { Comment } from "./Comment"
+// export { Reaction } from "./Reaction"
+
+export const entities = [User, Comment, Reaction]
