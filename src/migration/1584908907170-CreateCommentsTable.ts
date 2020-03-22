e2a621e214b0a3df058851d2f25e136c378fd64e
Kevin Wang <kwangsan@gmail.com>
2020-03-22 16:43:33 -0400

    CreateCommentsTable migration - CreateCommentsTable1584908907170

---
+++ src/migration/1584908907170-CreateCommentsTable.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateCommentsTable1584908907170 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
+			CREATE TABLE "Comments"
+			(
+				id SERIAL PRIMARY KEY,
+				type VARCHAR NOT NULL DEFAULT 'Comment',
+				body VARCHAR NOT NULL,
+				url VARCHAR NOT NULL,
+				-- NOW() is a postgres date function
+				created TIMESTAMP NOT NULL DEFAULT NOW(),
+				userId INTEGER NOT NULL,
+				deleted TIMESTAMP,
+				updated TIMESTAMP DEFAULT NOW()
+			)
+    `)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`
+			DROP TABLE "Comments"
+		`)
+  }
+}
