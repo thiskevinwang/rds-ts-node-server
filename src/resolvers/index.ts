463f87aff5137861695adfc0de19650910f20592
Kevin Wang <kwangsan@gmail.com>
2020-04-01 02:22:36 -0400

    export type ResolverFn

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,6 +1,14 @@
+import { Context } from "../../index"
 import * as Subscription from "./Subscription"
 import * as Query from "./Query"
 import * as Mutation from "./Mutation"
+
+export type ResolverFn<R = any, A = any> = (
+  parent: any,
+  args: A,
+  context: Context,
+  info: any
+) => Promise<R>
 
 export const resolvers = {
   Query,
