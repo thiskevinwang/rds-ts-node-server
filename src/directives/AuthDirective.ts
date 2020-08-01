6f24c2798dcb1cceaa31e8e998449218c06df553
Kevin Wang <kwangsan@gmail.com>
2020-08-01 10:09:04 -0400

    add directives

---
+++ src/directives/AuthDirective.ts
@@ -0,0 +1,31 @@
+import { SchemaDirectiveVisitor } from "apollo-server"
+import { GraphQLField, defaultFieldResolver } from "graphql"
+
+import { getUserId } from "utils"
+
+const AUTH_ERROR = "🔒 This field requires you to be authenticated"
+/**
+ * @usage
+ *
+ * ```graphql
+ * directive @auth on FIELD_DEFINITION
+ * ```
+ *
+ * @see https://www.apollographql.com/docs/graphql-tools/schema-directives/#examples
+ */
+export class AuthDirective extends SchemaDirectiveVisitor {
+  public visitFieldDefinition(field: GraphQLField<any, any>) {
+    const { resolve = defaultFieldResolver } = field
+    field.description = AUTH_ERROR
+
+    field.resolve = async function (...args) {
+      // Check Authorization Header
+      const context = args[2]
+      getUserId(context)
+
+      // Business as usual
+      const result = await resolve.apply(this, args)
+      return result
+    }
+  }
+}
