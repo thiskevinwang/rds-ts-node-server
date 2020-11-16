c907f7752a28f00bf0b6bd08d87efa4f13b1769f
Kevin Wang <kwangsan@gmail.com>
2020-11-15 22:42:25 -0500

    Schema: remove password related mutations

--- src/schema.ts
+++ src/schema.ts
@@ -134,42 +134,9 @@ export const typeDefs = gql`
 
   type Mutation {
     """
-    # signup
-    This mutation creates a new row in the **Users** table
-
-    Other entity columns:
-      - \`id\` is auto-incremented
-      - \`type\` defaults to "User"
-    """
-    signup(
-      email: String!
-      password: String!
-      username: String!
-      firstName: String!
-      lastName: String!
-    ): AuthPayload
-
-    """
     Trade a code—appended by the Cognito Hosted UI—for Cognito Tokens
     """
     getToken(code: String!): AuthResponse
-
-    updatePassword(password: String!, newPassword: String!): AuthPayload
-
-    """
-    This should return nothing.
-    It should just have a side effect of sending a password reset email link
-    """
-    requestPasswordResetLink(
-      """
-      A email address, duh
-      """
-      email: String!
-    ): ResetPasswordResponse
-
-    resetPassword(password: String!): User
-
-    login(email: String!, password: String!): AuthPayload
 
     createComment(body: String!, url: String!): Comment
 
